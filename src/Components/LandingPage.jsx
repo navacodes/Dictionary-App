e567c79744039f607eb95382e814620effbcc62b
dnava24 <61266103+dnava24@users.noreply.github.com>
2024-03-16 23:20:38 -0500

    Update text color in LandingPage component

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -4,7 +4,7 @@ const LandingPage = () => {
 			<h1 className='text-4xl md:text-6xl font-bold text-secondary mb-4 text-center'>
 				Welcome to the Dictionary App!
 			</h1>
-			<p className='text-sm md:text-lg text-gray-400 text-center md:p-10'>
+			<p className='text-sm md:text-lg text-gray-300 text-center md:p-10'>
 				Explore the world of words like never before with our{' '}
 				<span className='text-accent'>Dictionary App!</span> Look up definitions, hear pronunciations,
 				and discover different word from verbs, noun, and sentence usage. Plus, personalize your
